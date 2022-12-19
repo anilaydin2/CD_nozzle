e00801b052a8310e0b9bae5bd1be3558cb7d0109
anilaydin2 <121007844+anilaydin2@users.noreply.github.com>
2022-12-19 09:55:29 +0300

    Update design_param_for_thrust.m

--- design_param_for_thrust.m
+++ design_param_for_thrust.m
@@ -8,9 +8,9 @@ h = 1000.0; % m
 R = 286.9; %J/kgK 
 
 % User defined values
-p_total = input("Inlet Pressure [Pa]: "); %Pa
-r_inlet = input("Inlet radius [m]: ");
-thrust = input("Wanted thrust [N]: ");
+p_total = input("Inlet Pressure [40e6Pa]: "); %Pa
+r_inlet = input("Inlet radius [7.48m]: ");
+thrust = input("Wanted thrust [25N]: ");
 
 %Calculations
 A_inlet = pi*r_inlet^2; %m
